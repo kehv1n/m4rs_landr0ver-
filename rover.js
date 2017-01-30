30b481d1a278c30f0b59d13e5eb7ea8c5308a1f8
Kevin D. Silva <kevin.dasilva1@gmail.com>
2017-01-30 08:24:39 -0500

    Finished the rover. Implemented forward, backward, left, and right movement and rock detecter. Rover /somewhat/ complete..
    git status
    ?

--- rover.js
+++ rover.js
@@ -1,173 +1,181 @@
+//Define and store properties of the rover as it moves
 var myRover = {
     position: [0, 0],
     direction: 'N'
 };
 
+//Define the location of the rocks.  Rocks should stay in place.
 var myRock = [
     [5, 5],
     [4, 4],
     [9, 9],
-    [1, 1]
+    [4, 0]
 ];
 
-function goForward(rover) {
+  function goForward(rover) {
     switch (rover.direction) {
-        case 'N':
-            rover.position[0]++;
+          case 'N':
+              rover.position[0]++;
+              //check to see if rover is still on the grid
+              loopDeLoop(rover);
+              //checkRock() needs the parenthesis to run as a function
+              if (checkRock() === true) {
+                rover.position[0]--;
+              }
+              break;
 
-            checkRock(myRock, myRover);
+          case 'E':
+              rover.position[1]++;
 
-            immediateDeath(rover);
-            break;
 
-        case 'E':
-            rover.position[1]++;
-            immediateDeath(rover);
+              break;
+          case 'S':
+              rover.position[0]--;
 
-            break;
-        case 'S':
-            rover.position[0]--;
-            immediateDeath(rover);
-            break;
-        case 'W':
-            rover.position[1]--;
-            immediateDeath(rover);
-            break;
+              break;
+          case 'W':
+              rover.position[1]--;
+
+              break;
+
+      }
+      //include thses lines in the goForward function
+      console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]");
+      console.log("New Rover Direction: ["+ myRover.direction +"]");
     }
 
-    console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
-}
-
-function goBackward(rover) {
+  function goBackward(rover) {
     switch (rover.direction) {
-        case 'N':
-
-            var currentPosition = [rover.position[0], rover.position[1]];
-
-            rover.position[0]--;
-
-            checkRock(myRock, myRover);
-            immediateDeath(rover);
+          case 'N':
 
 
-
-            break;
-
-        case 'E':
-            rover.position[1]--;
-            immediateDeath(rover);
-            break;
-        case 'S':
-            rover.position[0]++;
-            immediateDeath(rover);
-            break;
-        case 'W':
-            rover.position[1]++;
-            immediateDeath(rover);
-            break;
-    }
-
-    console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
-}
-
-
-function turnRight(rover) {
-    switch (rover.direction) {
-        case 'N':
-            rover.direction = 'E';
-            break;
-        case 'E':
-            rover.position = 'S';
-            break;
-        case 'S':
-            rover.position = 'W';
-            break;
-        case 'W':
-            rover.position = 'N';
-            break;
-    }
-
-    console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
-}
-
-function turnLeft(rover) {
-    switch (rover.direction) {
-        case 'N':
-            rover.direction = 'W';
-            break;
-        case 'E':
-            rover.position = 'N';
-            break;
-        case 'S':
-            rover.position = 'E';
-            break;
-        case 'W':
-            rover.position = 'S';
-            break;
-    }
-
-
-    console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
-}
-
-var myRock = [
-    [5, 5],
-    [4, 4],
-    [9, 9],
-    [1, 1]
-];
-
-function checkRock(myRock, myRover) { /// LOOKIN FOR PEBBLES
-
-
-    for (var i = 0; i <= myRock.length - 1; i++) {
-        if ((myRover.position[0] === myRock[i][0]) && (myRover.position[1] === myRock[i][1])) {
-            console.log("you hit a rock damnit!");
-
-
-
-        }
-    }
-}
-
-
-function userInput() { //WHERE WE GOIN?
-
-    var nextDir = prompt("Where do you want the rover to move (w, s, d, a)?: ");
-    // for (var i = 0; i <= 10; i++)
-
-    switch (nextDir) {
-
-        case "w":
-            goForward(myRover);
-            break;
-
-        case "d":
-            turnRight(myRover);
-
-            break;
-
-        case "s":
-            goBackward(myRover);
-            break;
-
-        case "a":
-            turnRight(myRover);
-
-            break;
-
-
-
-
+              rover.position[0]--;
+              //Added code for Case N from goForward function
+              //check to see if rover is still on the grid
+              loopDeLoop(rover);
+              //checkRock() needs the parenthesis to run as a function
+              if (checkRock() === true) {
+                rover.position[0]++;
+              }
+              break;
+          case 'E':
+              rover.position[1]--;
+              break;
+          case 'S':
+              rover.position[0]++;
+              break;
+          case 'W':
+              rover.position[1]++;
+              break;
+      }
+      console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+      console.log("New Rover Direction: ["+ rover.direction +"]");
 
     }
 
-}
 
-function immediateDeath(rover) {
+
+
+
+
+  function turnRight(rover) {
+      switch (rover.direction) {
+          case 'N':
+              rover.direction = 'E';
+              break;
+          case 'E':
+              rover.direction = 'S';
+              break;
+          case 'S':
+              rover.direction = 'W';
+              break;
+          case 'W':
+              rover.direction = 'N';
+              break;
+      }
+
+      console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+      console.log("New Rover Direction: ["+ rover.direction +"]");
+  }
+
+  function turnLeft(rover) {
+      switch (rover.direction) {
+          case 'N':
+              rover.direction = 'W';
+              break;
+          case 'E':
+              rover.direction = 'N';
+              break;
+          case 'S':
+              rover.direction = 'E';
+              break;
+          case 'W':
+              rover.direction = 'S';
+              break;
+      }
+
+
+      console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+      console.log("New Rover Direction: ["+ rover.direction +"]");
+  }
+
+  function checkRock() { /// LOOKIN' FOR PEBBLES
+      for (var i = 0; i <= myRock.length - 1; i++) {
+          if ((myRover.position[0] === myRock[i][0]) && (myRover.position[1] === myRock[i][1])){
+              console.log("you hit a rock damnit!");
+              return true;
+            }
+          }
+  }
+
+  //Updated userInput function
+  //This function does not need any attributes i.e. (rover) passed to it
+  //Added cases for capital and lowercase input.
+  function userInput() {
+    var input = prompt("Where would you like to go?");
+    switch(input) {
+      case 'f':
+        goForward(myRover);
+        userInput();
+        break;
+      case 'r':
+        turnRight(myRover);
+        userInput();
+          break;
+      case 'b':
+        goBackward(myRover);
+        userInput();
+          break;
+      case 'l':
+        turnLeft(myRover);
+        userInput();
+          break;
+      case 'F':
+        goForward(myRover);
+        userInput();
+          break;
+      case 'R':
+        turnRight(myRover);
+        userInput();
+          break;
+      case 'B':
+        goBackward(myRover);
+        userInput();
+          break;
+      case 'L':
+        turnLeft(myRover);
+        userInput();
+          break;
+      case 'q':
+        break;
+      default:
+        break;
+    }
+  }
+
+  function loopDeLoop(rover) {
     if (rover.position[0] > 9) {
         rover.position[0] = 0;
-
     } else if (rover.position[0] < 0) {
         rover.position[0] = 9;
     } else if (rover.position[1] > 9) {
@@ -178,4 +186,9 @@ function immediateDeath(rover) {
 
 }
 
+//Start the program
 userInput();
+
+//first project at ironhack complete by:
+//: Jan 30, 2017.
+//Thank you <b>friends</b> for the great help.
